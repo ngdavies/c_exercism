0d336c0f19ce94287faec64c643d53c1a6e801ff
Nigel Davies <ngdavies@hotmail.com>
2021-02-19 14:38:37 +0000

    isograms using pointers rather than arrays. Not so clear to me.

--- isogram/src/isogram.c
+++ isogram/src/isogram.c
@@ -1,20 +1,18 @@
 #include "isogram.h"
-#include "string.h"
+#include <ctype.h>
 
 bool is_isogram(const char phrase[]) {
   if (!phrase) {
     return false;
   }
 
-  size_t len = strlen(phrase); 
   bool seen[26] = {false};
-  for (size_t i=0; i<len; i++) {
-    char ch = phrase[i];
+  for (; *phrase; phrase++) {
     size_t ix;
-    if (ch>='A' && ch<='Z') {
-      ix = ch - 'A';
-    } else if (ch>='a' && ch<='z') {
-      ix = ch - 'a';
+    if (isupper(*phrase)) {
+      ix = *phrase - 'A';
+    } else if (islower(*phrase)) {
+      ix = *phrase - 'a';
     } else {
        continue;
     }
